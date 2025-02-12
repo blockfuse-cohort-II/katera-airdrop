d1828c120ae096de7cbf861e75bd47790beacd3a
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-02-12 23:35:15 +0100

    basic structure of airdrop

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -1,7 +1,7 @@
 const Body = () => {
   return (
     <div className="">
-      <div className="w-1/2 m-auto items-center pt-32 text-center">
+      <div className="md:w-1/2 lg:w-1/3 xl:1/3 w-2/3 m-auto items-center pt-32 text-center">
         <form action="">
           <div className="border border-slate-400 rounded">
             <input type="text" className="w-full py-1.5 rounded px-3 outline-violet-400" />
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,13 @@ import logo from "../assets/logo2.png"
 
 const Navbar = () => {
   return (
-    <div className="py-5 px-16 flex justify-between shadow-md">
+    <div className="lg:py-5 lg:px-16 flex py-3  px-5 justify-between shadow-md">
       <div>
          <img src={logo} alt="katera logo" width={35}/>
       </div>
 
-      <div>
-         <button className="bg-violet-500 px-5 py-3 rounded text-sm font-semibold text-white">Connect wallet</button>
+      <div className="self-center">
+         <button className="bg-violet-500 lg:px-5 lg:py-3  py-1 px-2  rounded text-sm font-semibold text-white">Connect wallet</button>
       </div>
     </div>
   )
