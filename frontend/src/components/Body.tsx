7dd3093028b3c71f9b40c67d2eb6e0d49d31c14a
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-02-12 23:28:55 +0100

    basic structure of airdrop

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -1,13 +1,13 @@
 const Body = () => {
   return (
-    <div>
-      <div>
+    <div className="">
+      <div className="w-1/2 m-auto items-center pt-32 text-center">
         <form action="">
-          <div>
-            <input type="text" />
+          <div className="border border-slate-400 rounded">
+            <input type="text" className="w-full py-1.5 rounded px-3 outline-violet-400" />
           </div>
           <div>
-            <button>Claim Airdrop</button>
+            <button className="bg-violet-500 w-full mt-5 text-white py-2 rounded">Claim Airdrop</button>
           </div>
         </form>
       </div>
