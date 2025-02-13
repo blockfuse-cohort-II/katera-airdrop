1a977cc2eec3d7110252f273cd1d6bac925fe5f6
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-02-13 03:04:27 +0100

    design for today

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import logo from "../assets/logo2.png";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className="lg:py-4 lg:px-10 flex py-3  px-5 justify-between shadow-md  w-4/5 m-auto rounded-full bg-[#2A2A2A]/90">
+    <div className="lg:py-4 lg:px-10 flex py-3  px-5 justify-between shadow-md  w-4/5 m-auto rounded-full bg-[#252A30]/90">
       <div className="flex">
         <div className="self-center">
-          <Link to="">
+          <Link to="/">
             <img src={logo} alt="katera logo" width={30} />
           </Link>
         </div>
@@ -15,9 +16,20 @@ const Navbar = () => {
         </div>
       </div>
       <div className="self-center">
-        <button className="bg-violet-500 lg:px-5 lg:py-2  py-1 px-2  rounded text-sm font-semibold text-white">
+        <motion.button
+          className="bg-violet-500 lg:px-5 lg:py-2  py-1 px-2  rounded text-sm font-semibold text-white"
+          whileHover={{
+            scale: 1.05, // Slightly enlarges on hover
+            backgroundColor: "#4F46E5", // Changes color on hover
+            boxShadow: "0px 10px 20px rgba(79, 70, 229, 0.3)", // Adds a glow effect
+          }}
+          whileTap={{
+            scale: 0.9, // Shrinks slightly on click
+          }}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
           Connect wallet
-        </button>
+        </motion.button>
       </div>
     </div>
   );
