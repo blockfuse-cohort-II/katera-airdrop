2bf4766ccab21136a0647ec171be95ed819411e7
Sprof22 <richmondelaigwu@gmail.com>
2025-02-14 14:15:01 +0100

    revert

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -6,7 +6,7 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
 
-    const allocation = [{ amount: 100, user: account1.address }, { amount: 200, user: account2.address }, { amount: 300, user: account3.address }, { amount: 400, user: account4.address }];
+    const allocation = [{ amount: 1000, user: '0xa38062B76617585a6DB4AF9759ef3A850B35Ed9a' }, { amount: 1000, user: '0xDf6Eab507dfD58568C4C782D7669aC4bFC895D77' }, { amount: 1000, user: '0xe44326D3d3F85d5FC675b4e65055a0e9C575d5E0' }, { amount: 1000, user: '0xc23f538d15950f6982aA8e0e3a5bc1E1DcBB3177' }, { amount: 1000, user: '0xeb7CD716C5018AC5a0cF13Ee55F15EB9B4038B41' }, { amount: 1000, user: '0xd65944287EB2685c345057F6a4A48d619bA6f7cf'}];
 
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
